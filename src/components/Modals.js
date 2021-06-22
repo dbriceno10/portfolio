ce9d88d4dec9d304297407255f4c48aee54fdd20
nombre <daniel.b100726@gmail.com>
2021-06-22 17:33:00 -0400

    Agregado proyecto 3

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -10,11 +10,14 @@ import Modal01 from "../assets/image/screenshot/batata.jpg"
 import Proyect01 from "./modals/Proyect01"
 import Modal02 from "../assets/image/screenshot/spacebadges.jpg"
 import Proyect02 from "./modals/Proyect02"
+import Modal03 from "../assets/image/screenshot/productapp.jpg"
+import Proyect03 from "./modals/Proyect03"
 
 
 const Modals = () => {
     const [isOpenModal1, openModal1, closeModal1] = useModal(false)
     const [isOpenModal2, openModal2, closeModal2] = useModal(false)
+    const [isOpenModal3, openModal3, closeModal3] = useModal(false)
 
     return(
         <div className="row collapse block-large-1-4 block-medium-1-3 block-tab-1-2 block-500-stack folio-list">
@@ -26,6 +29,11 @@ const Modals = () => {
             <ModalElement image={Modal02} e={openModal2} />
             <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
                 <Proyect02 />
+            </Modal>
+
+            <ModalElement image={Modal03} e={openModal3} />
+            <Modal isOpen={isOpenModal3} closeModal={closeModal3}>
+                <Proyect03 />
             </Modal>
         </div>
     )
---
+++ src/components/modals/Proyect03.js
@@ -0,0 +1,26 @@
+import React from "react"
+import image from "../../assets/image/screenshot/productapp-baner.png"
+import "../styles/Modal.css"
+
+const Proyect03 = () => {
+    return(
+        <React.Fragment>
+            <div className="modal-internal-container">
+                <img src={image} alt="image" className="" />
+                <div className="">
+                    <p className="modal__title">E-commerce Product App</p>
+                    <p>
+                    A simple E-commerce type product app, with quick bootstrap styles, fully responsive.<br />Technologies applied: HTML, CSS, JavaScrip, Git/Github, Bootstrap.
+                    </p>
+                    <div className="link-container">
+                        <a href="https://dbriceno10.github.io/aplicacion-de-productos-2/" target="__blank" className="btn btn_primary">Visit Page</a>
+                    </div>
+                </div>               
+            </div>
+        </React.Fragment>
+    )
+}
+
+
+
+export default Proyect03
