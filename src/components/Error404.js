99351654d5e24cf26127b22f0009361dd7275e6d
nombre <daniel.b100726@gmail.com>
2021-06-23 10:50:15 -0400

    agregado componente Error

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,8 +1,43 @@
 import React from "react"
+import { Link } from "react-router-dom" 
+import "./styles/Error.css"
+import error500 from "../assets/image/Error404.jpg"
 
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="s-header layout">
+                <div className="row s-header__nav-wrap">
+                    <nav className="s-header__nav">
+                        <ul>
+                            <li id="home"><Link className="smoothscroll" to="/">Home</Link></li>
+                            <li id="about"><Link className="smoothscroll" to="/about">About</Link></li>
+                            <li id="portfolio"><Link className="smoothscroll" to="/portfolio">Portfolio</Link></li>
+                            <li id="contact"><Link className="smoothscroll" to="/contact">Contact</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        )
+    }
+  }
 class Error404 extends React.Component {
     render() {
-        return <h1>Error404 Not Found</h1>
+        return(
+            <React.Fragment>
+            <Header />
+                <div className="PageError">
+            <Link to={`/`}>
+              <img className="error-img" src={error500} alt="server-internal error" />
+            </Link>
+            {/* <h1 className="h1-title">Error404 Not Found.</h1> */}
+            <div className="Badges__buttons">
+              <Link className="btn btn-primary" to={`/`}>Home</Link>
+            </div>
+          </div>
+            </React.Fragment>
+        )
     }
 }
 
