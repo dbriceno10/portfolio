0740fa87ce05e2c765b914ae318a57c4cde07234
nombre <daniel.b100726@gmail.com>
2021-09-16 08:30:36 -0400

    Formateo general del código y limpieza

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route, HashRouter } from "react-router-dom";
-// import Layout from "./components/layout";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
@@ -11,17 +10,13 @@ import "./global.css";
 const App = () => {
   return (
     <HashRouter>
-      {/* <Layout> */}
       <Switch>
         <Route exact path="/" component={Hero} />
         <Route exact path="/about" component={About} />
-        {/* <Route exact path="/resume" component={Resume} /> */}
         <Route exact path="/portfolio" component={Portfolio} />
-        {/* <Route exact path="/testimonials" component={Testimonials} /> */}
         <Route exact path="/contact" component={Contactus} />
         <Route path="*" component={Error404} />
       </Switch>
-      {/* </Layout> */}
     </HashRouter>
   );
 };
