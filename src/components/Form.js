c5bf41d755a3b7e61876f8a17f2ec543d18b2b97
nombre <daniel.b100726@gmail.com>
2021-09-25 11:48:27 -0400

    Probando el formulario controlado

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,56 @@ import React from "react";
 import "./styles/From.css";
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullname: "",
+      email: "",
+      message: "",
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const values = JSON.stringify(this.state);
+    console.log(values)
+    alert(values);
+  };
+
   render() {
+    const { fullname, email, message } = this.state;
     return (
       <React.Fragment>
-        <form id="valid-form">
+        <form id="valid-form" onSubmit={this.handleSubmit}>
           <div className="inputs-container">
             <div>
-              <p className="form-paceholder">Full Name</p>
+              <label className="form-paceholder">Full Name</label>
               <input
                 id="fullname"
                 type="text"
+                name="fullname"
+                value={fullname}
                 placeholder="Full Name"
+                onChange={this.handleChange}
                 required
               />
             </div>
             <div>
-              <p className="form-paceholder">Email</p>
-              <input id="email" type="email" placeholder="Email" required />
+              <label className="form-paceholder">Email</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={email}
+                placeholder="Email"
+                onChange={this.handleChange}
+                required
+              />
             </div>
           </div>
           <div className="textarea-container">
@@ -26,10 +59,13 @@ class Form extends React.Component {
               id="message"
               placeholder="Message"
               maxLength="2000"
+              name="message"
+              value={message}
+              onChange={this.handleChange}
             ></textarea>
           </div>
           <div className="btn-form-container">
-            <buton className="btn btn-primary send">Send</buton>
+            <input Value="Send" type="submit" className="btn btn-primary send"/>
           </div>
         </form>
       </React.Fragment>
