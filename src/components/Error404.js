e0220b22737849e07f7a52c572b5aff7c861a86d
Daniel Briceño <daniel.b100726@gmail.com>
2021-10-23 18:58:10 -0400

    Convertida la página de error en componente funcional y ajustado el padding del boton que lleva al home en el mismo

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 import "./styles/Error.css";
 import error500 from "../assets/image/Error404.jpg";
 
-class Error404 extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="PageError">
-          <Link to={`/`}>
-            <img
-              className="error-img"
-              src={error500}
-              alt="server-internal error"
-            />
+const Error404 = () => {
+  return (
+    <React.Fragment>
+      <div className="PageError">
+        <Link to={`/`}>
+          <img
+            className="error-img"
+            src={error500}
+            alt="server-internal error"
+          />
+        </Link>
+        <div className="Badges__buttons">
+          <Link className="btn btn-primary btn-home" to={`/`}>
+            Home
           </Link>
-          <div className="Badges__buttons">
-            <Link className="btn btn-primary btn-home" to={`/`}>
-              Home
-            </Link>
-          </div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Error404;
