a59918c87b916be6cdacb5ef23285d5676f7de7f
Daniel Briceño <daniel.b100726@gmail.com>
2022-01-28 19:01:35 -0400

    agregando twitter, cambiando la fuente de texto

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -6,89 +6,101 @@ import phoneStiker from "../assets/image/1220365_whatsapp_chat_communication_mes
 import githubStiker from "../assets/image/github (3).png";
 import linkedinStiker from "../assets/image/linkedin.png";
 import telegranStiker from "../assets/image/telegram-logo.png";
+import twitterStiker from "../assets/image/twitter.png";
 import ContactNetwork from "./ContactNetwork";
 const cellPhone = "https://api.whatsapp.com/send/?phone=584249501974&text&app_absent=0";
 const githubLink = "https://github.com/dbriceno10";
 const linkedinLink = "https://www.linkedin.com/in/dbriceno10/";
 const telegramLink = "https://t.me/dbriceno";
+const twitterLink = "https://twitter.com/dbriceno10dev";
 
 const Contactus = () => {
   return (
-      <section id="contact" className="s-contact target-section">
-        <div className="row s-contact__header">
-          <div className="column large-12">
-            <h3 className="section-header-allcaps">
-              <span className="black-background">{/*Say Hello*/}Contáctame</span>
-            </h3>
-          </div>
+    <section id="contact" className="s-contact target-section">
+      <div className="row s-contact__header">
+        <div className="column large-12">
+          <h3 className="section-header-allcaps">
+            <span className="black-background">{/*Say Hello*/}Contáctame</span>
+          </h3>
         </div>
-        <div className="row s-contact__content">
-          <div className="column large-7 medium-12">
-            <h4 className="huge-text">
-              <span className="black-background">
-                {/* Let's start a new project together, I want to collaborate to
+      </div>
+      <div className="row s-contact__content">
+        <div className="column large-7 medium-12">
+          <h4 className="huge-text">
+            <span className="black-background">
+              {/* Let's start a new project together, I want to collaborate to
                 materialize that idea into a successful project. */}
-                Estoy listo para iniciar un nuevo proyecto y ayudarte a llevar a cabo tu idea.
-              </span>
-            </h4>
-            <Form />
-          </div>
-          <div className="column large-4 medium-12">
-            <div className="row contact-infos">
-              <div
-                className="column large-12 medium-6 tab-12"
-                id="ContactNetwork-Container"
-              >
-                <div className="contact-block">
-                  <ContactNetwork
-                    stiker={gmailStiker}
-                    alt="gmail-Stiker"
-                    title="Gmail Stiker"
-                    header="Email"
-                    linkName="dbriceno10dev@gmail.com"
-                  />
-                  <ContactNetwork
-                    stiker={phoneStiker}
-                    alt="phone-Stiker"
-                    title="Phone Stiker"
-                    header="WhatsApp"
-                    href={cellPhone}
-                    target="_blank"
-                    linkName="Enlace al Chat"
-                  />
-                  <ContactNetwork
-                    stiker={githubStiker}
-                    alt="github-Stiker"
-                    title="Github Stiker"
-                    header="Github"
-                    href={githubLink}
-                    target="_blank"
-                    linkName="dbriceno10"
-                  />
-                  <ContactNetwork
-                    stiker={linkedinStiker}
-                    alt="linkedin-Stiker"
-                    title="Linkedin Stiker"
-                    header="Linkedin"
-                    href={linkedinLink}
-                    target="_blank"
-                    linkName="Daniel Briceño"
-                  />
-                  <ContactNetwork
-                    stiker={telegranStiker}
-                    alt="telegram-Stiker"
-                    title="Telegram Stiker"
-                    header="Telegram"
-                    href={telegramLink}
-                    target="_blank"
-                    linkName="@dbriceno"
-                  />
-                </div>
+              Estoy listo para iniciar un nuevo proyecto y ayudarte a llevar a
+              cabo tu idea.
+            </span>
+          </h4>
+          <Form />
+        </div>
+        <div className="column large-4 medium-12">
+          <div className="row contact-infos">
+            <div
+              className="column large-12 medium-6 tab-12"
+              id="ContactNetwork-Container"
+            >
+              <div className="contact-block">
+                <ContactNetwork
+                  stiker={gmailStiker}
+                  alt="gmail-Stiker"
+                  title="Gmail Stiker"
+                  header="Email"
+                  linkName="dbriceno10dev@gmail.com"
+                />
+                <ContactNetwork
+                  stiker={linkedinStiker}
+                  alt="linkedin-Stiker"
+                  title="Linkedin Stiker"
+                  header="Linkedin"
+                  href={linkedinLink}
+                  target="_blank"
+                  linkName="Daniel Briceño"
+                />
+                <ContactNetwork
+                  stiker={githubStiker}
+                  alt="github-Stiker"
+                  title="Github Stiker"
+                  header="Github"
+                  href={githubLink}
+                  target="_blank"
+                  linkName="dbriceno10"
+                />
+                <ContactNetwork
+                  stiker={twitterStiker}
+                  alt="twitter-Stiker"
+                  title="Twitter Stiker"
+                  header="Twitter"
+                  href={twitterLink}
+                  target="_blank"
+                  linkName="@dbriceno10dev"
+                />
+                <ContactNetwork
+                  stiker={phoneStiker}
+                  alt="phone-Stiker"
+                  title="Phone Stiker"
+                  header="WhatsApp"
+                  href={cellPhone}
+                  target="_blank"
+                  linkName="Enlace al Chat"
+                />
+                <ContactNetwork
+                  stiker={telegranStiker}
+                  alt="telegram-Stiker"
+                  title="Telegram Stiker"
+                  header="Telegram"
+                  href={telegramLink}
+                  target="_blank"
+                  linkName="@dbriceno"
+                />
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,55 +1,55 @@
 import React from "react";
 import logoGithub from "../assets/image/github (3).png";
 import logoLinkedin from "../assets/image/linkedin.png";
-import logoTelegram from "../assets/image/telegram-logo.png";
+import logoTwitter from "../assets/image/twitter.png";
 import HeroButton from "./HeroButton";
 import HeroSocial from "./HeroSocial";
 import "./styles/Hero.css";
 const linkedinLink = "https://www.linkedin.com/in/dbriceno10/";
 const gihubLink = "https://github.com/dbriceno10";
-const telegramLink = "https://t.me/dbriceno";
+const twitterLink = "https://twitter.com/dbriceno10dev";
 
 const Hero = () => {
   return (
-      <section id="hero" className="s-hero target-section">
-        <div className="s-hero__bg rellax" data-rellax-speed={-7} />
-        <div className="row s-hero__content">
-          <div className="column">
-            <div className="s-hero__content-about">
-              <h1>
-                <span className="font-500">Daniel Briceño</span>
-              </h1>
-              <h3>
-                <span>Full-Stack Web Developer.</span>
-              </h3>
-              <div className="s-hero__content-social">
-                <HeroSocial
-                  href={linkedinLink}
-                  src={logoLinkedin}
-                  alt="logo de linkedin"
-                  title="my linkedin"
-                />
-                <HeroSocial
-                  href={gihubLink}
-                  src={logoGithub}
-                  alt="logo de github"
-                  title="my github"
-                />
-                <HeroSocial
-                  href={telegramLink}
-                  src={logoTelegram}
-                  alt="logo de telegram"
-                  title="Telegram: @dbriceno"
-                />
-              </div>
+    <section id="hero" className="s-hero target-section">
+      <div className="s-hero__bg rellax" data-rellax-speed={-7} />
+      <div className="row s-hero__content">
+        <div className="column">
+          <div className="s-hero__content-about">
+            <h1>
+              <span className="font-500">Daniel Briceño</span>
+            </h1>
+            <h3>
+              <span>Front-End && Full-Stack Web Developer</span>
+            </h3>
+            <div className="s-hero__content-social">
+              <HeroSocial
+                href={linkedinLink}
+                src={logoLinkedin}
+                alt="logo de linkedin"
+                title="LinkedIn"
+              />
+              <HeroSocial
+                href={gihubLink}
+                src={logoGithub}
+                alt="logo de github"
+                title="Github"
+              />
+              <HeroSocial
+                href={twitterLink}
+                src={logoTwitter}
+                alt="logo de telegram"
+                title="Twitter"
+              />
             </div>
           </div>
         </div>
-        <div className="s-hero__scroll links">
-          <HeroButton path="/contact" text="Hire Me" />
-          <HeroButton path="/portfolio" text="Explore Me" />
-        </div>
-      </section>
+      </div>
+      <div className="s-hero__scroll links">
+        <HeroButton path="/contact" text="Hire Me" />
+        <HeroButton path="/portfolio" text="Explore Me" />
+      </div>
+    </section>
   );
 };
 
